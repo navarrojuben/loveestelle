daa38af71f31c6956a0fcedcbc41760d992e4784
Juben Alexis Navarro <juben.lnavarro@gmail.com>
2021-05-29 10:57:25 +0800

    Trial-two

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>HELLO, ESTELLE - trial-one</h1>
+      <h1>HELLO, ESTELLE - trial-two</h1>
     </div>
   );
 }
